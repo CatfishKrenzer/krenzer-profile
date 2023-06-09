57156d7083db7d9e045b8efc90cee99c4dc8290e
kre7276 <kre7276@northwesternmutual.com>
2023-06-09 15:29:23 -0500

    chore: convert to webpack

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "[name].js",
+    clean: true,
+    publicPath:'/',
+  },
+  devtool: "source-map",
+  devServer: {
+    historyApiFallback:true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        exclude: /node_modules/,
+        use: ["file-loader?name=[name].[ext]"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+      filename: "index.html"
+    }),
+  ],
+};
